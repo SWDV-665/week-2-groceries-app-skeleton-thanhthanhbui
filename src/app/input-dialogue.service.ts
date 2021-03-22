15444d8471aa4fd2aa4b860df5a2fa3212484d1d
Thanh Thanh Bui <47639612+thanhthanhbui@users.noreply.github.com>
2021-03-21 18:20:05 -0600

    First commit

--- src/app/input-dialogue.service.ts
+++ src/app/input-dialogue.service.ts
@@ -12,44 +12,5 @@ export class InputDialogueService {
     console.log('Utilizing InputDialogue Service');
   }
 
-  async showPrompt(item?, index?) {
-    const alert = await this.alertController.create({
-      // cssClass: 'my-custom-class',
-      header: 'Add Item',
-      // subHeader: 'Subtitle',
-      message: 'Please add item',
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Item',
-          value: item? item.name : null
-        },
-        {
-          name: 'qty',
-          placeholder: 'Quantity',
-          value: item? item.qty : null
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          // role: 'cancel',
-          // cssClass: 'secondary',
-          handler: (item) => {
-            console.log('Cancel item entry');
-          }
-        }, 
-        {
-          text: 'Save',
-          handler: (item) => {
-            console.log('Save', item);
-            this.dataSvc.addItem(item);       
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
 
 }
